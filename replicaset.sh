17d8970a6d71e836901ca2785650adacd7eaab57
Apurba Samanta <apu1111@gmail.com>
2021-02-16 07:45:47 +0530

    #818 MongoDB IPv6 support (#12)
    Signed-off-by: Apurba Samanta <apu1111@gmail.com>

--- replicaset.sh
+++ replicaset.sh
@@ -58,26 +58,26 @@ fi
 log "Peers: ${peers[*]}"
 
 log "Waiting for MongoDB to be ready..."
-retry mongo --host localhost "${ssl_args[@]}" --eval "db.adminCommand('ping')"
+retry mongo --ipv6 --host localhost "${ssl_args[@]}" --eval "db.adminCommand('ping')"
 
 # check rs.isMaster() (we can run this without authentication) on self to see it is ready
-retry mongo admin --host localhost "${ssl_args[@]}" --quiet --eval "JSON.stringify(rs.isMaster())"
+retry mongo admin --ipv6 --host localhost "${ssl_args[@]}" --quiet --eval "JSON.stringify(rs.isMaster())"
 
 # check rs.isMaster() on each peer to see each of them are ready
 for peer in "${peers[@]}"; do
-    retry mongo admin --host "$peer" "${ssl_args[@]}" --quiet --eval "JSON.stringify(rs.isMaster())"
+    retry mongo admin --ipv6 --host "$peer" "${ssl_args[@]}" --quiet --eval "JSON.stringify(rs.isMaster())"
 done
 
 log "Initialized."
 sleep $DEFAULT_WAIT_SECS
 
-if [[ $(mongo admin --host localhost "${admin_creds[@]}" "${ssl_args[@]}" --quiet --eval "rs.status().myState") == '1' ]]; then
+if [[ $(mongo admin --ipv6 --host localhost "${admin_creds[@]}" "${ssl_args[@]}" --quiet --eval "rs.status().myState") == '1' ]]; then
     log "($service_name) is already master"
     log "Good bye."
     exit 0
 fi
 
-if [[ $(mongo admin --host localhost "${admin_creds[@]}" "${ssl_args[@]}" --quiet --eval "rs.status().myState") == '2' ]]; then
+if [[ $(mongo admin --ipv6 --host localhost "${admin_creds[@]}" "${ssl_args[@]}" --quiet --eval "rs.status().myState") == '2' ]]; then
     log "($service_name) is already added in replicaset"
     log "Good bye."
     exit 0
@@ -86,20 +86,20 @@ fi
 # try to find a master and add yourself to its replica set.
 for peer in "${peers[@]}"; do
     # re-check rs.isMaster() on the peer to see it is ready
-    retry mongo admin --host "$peer" "${admin_creds[@]}" "${ssl_args[@]}" --quiet --eval "JSON.stringify(rs.isMaster())"
-    out=$(mongo admin --host "$peer" "${admin_creds[@]}" "${ssl_args[@]}" --quiet --eval "JSON.stringify(rs.isMaster())")
+    retry mongo admin --ipv6 --host "$peer" "${admin_creds[@]}" "${ssl_args[@]}" --quiet --eval "JSON.stringify(rs.isMaster())"
+    out=$(mongo admin --ipv6 --host "$peer" "${admin_creds[@]}" "${ssl_args[@]}" --quiet --eval "JSON.stringify(rs.isMaster())")
     log "$out"
     if echo "$out" | jq -r '.ismaster' | grep 'true'; then
         log "Found master: $peer"
 
         # Retrying command until successful
         log "Adding myself ($service_name) to replica set..."
-        retry mongo admin --host "$peer" "${admin_creds[@]}" "${ssl_args[@]}" --quiet --eval "JSON.stringify(rs.add('$service_name'))"
+        retry mongo admin --ipv6 --host "$peer" "${admin_creds[@]}" "${ssl_args[@]}" --quiet --eval "JSON.stringify(rs.add('$service_name'))"
 
         sleep $DEFAULT_WAIT_SECS
 
         log 'Waiting for replica to reach SECONDARY state...'
-        until printf '.' && [[ $(mongo admin --host localhost "${admin_creds[@]}" "${ssl_args[@]}" --quiet --eval "rs.status().myState") == '2' ]]; do
+        until printf '.' && [[ $(mongo admin --ipv6 --host localhost "${admin_creds[@]}" "${ssl_args[@]}" --quiet --eval "rs.status().myState") == '2' ]]; do
             sleep 1
         done
 
@@ -111,15 +111,15 @@ for peer in "${peers[@]}"; do
 done
 
 # else initiate a replica set with yourself.
-if mongo --host localhost "${ssl_args[@]}" --eval "rs.status()" | grep "no replset config has been received"; then
+if mongo --ipv6 --host localhost "${ssl_args[@]}" --eval "rs.status()" | grep "no replset config has been received"; then
     # Retrying command until successful
     log "Initiating a new replica set with myself ($service_name)..."
-    retry mongo --host localhost "${ssl_args[@]}" --quiet --eval "JSON.stringify(rs.initiate({'_id': '$replica_set', 'writeConcernMajorityJournalDefault': false, 'members': [{'_id': 0, 'host': '$service_name'}]}))"
+    retry mongo --ipv6 --host localhost "${ssl_args[@]}" --quiet --eval "JSON.stringify(rs.initiate({'_id': '$replica_set', 'writeConcernMajorityJournalDefault': false, 'members': [{'_id': 0, 'host': '$service_name'}]}))"
 
     sleep $DEFAULT_WAIT_SECS
 
     log 'Waiting for replica to reach PRIMARY state...'
-    until printf '.' && [[ $(mongo --host localhost "${ssl_args[@]}" --quiet --eval "rs.isMaster().ismaster") == 'true' ]]; do
+    until printf '.' && [[ $(mongo --ipv6 --host localhost "${ssl_args[@]}" --quiet --eval "rs.isMaster().ismaster") == 'true' ]]; do
         sleep 1
     done
 
@@ -127,7 +127,7 @@ if mongo --host localhost "${ssl_args[@]}" --eval "rs.status()" | grep "no repls
 
     if [[ "$AUTH" == "true" ]]; then
         log "Creating admin user..."
-        mongo admin --host localhost "${ssl_args[@]}" --quiet --eval "db.createUser({user: '$admin_user', pwd: '$admin_password', roles: [{role: 'root', db: 'admin'}]})"
+        mongo admin --ipv6 --host localhost "${ssl_args[@]}" --quiet --eval "db.createUser({user: '$admin_user', pwd: '$admin_password', roles: [{role: 'root', db: 'admin'}]})"
     fi
 
     # Initialize Part for KubeDB.
@@ -145,7 +145,7 @@ if mongo --host localhost "${ssl_args[@]}" --eval "rs.status()" | grep "no repls
                 ;;
             *.js)
                 log "$0: running $f 1"
-                log "$(mongo --host localhost --quiet "$MONGO_INITDB_DATABASE" "${admin_creds[@]}" "${ssl_args[@]}" "$f")"
+                log "$(mongo --ipv6 --host localhost --quiet "$MONGO_INITDB_DATABASE" "${admin_creds[@]}" "${ssl_args[@]}" "$f")"
                 ;;
             *) log "$0: ignoring $f" ;;
         esac
@@ -159,7 +159,7 @@ fi
 if [[ ${SSL_MODE} != "disabled" ]] && [[ -f "$client_pem" ]]; then
     #xref: https://docs.mongodb.com/manual/tutorial/configure-x509-client-authentication/#procedures
     log "Creating root user ${INJECT_USER} for SSL..."
-    mongo admin --host localhost "${admin_creds[@]}" "${ssl_args[@]}" --eval "db.getSiblingDB(\"\$external\").runCommand({createUser: \"${INJECT_USER}\",roles:[{role: 'root', db: 'admin'}],})"
+    mongo admin --ipv6 --host localhost "${admin_creds[@]}" "${ssl_args[@]}" --eval "db.getSiblingDB(\"\$external\").runCommand({createUser: \"${INJECT_USER}\",roles:[{role: 'root', db: 'admin'}],})"
 fi
 
 log "Good bye."
